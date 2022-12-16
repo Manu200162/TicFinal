6c2ee8b2991f88b7485a43af8d962fa4842efd1f
Manu200162 <manli200134@gmail.com>
2022-12-16 12:47:03 -0400

    Second change

--- app/src/main/java/com/upb/ticfinal/MainActivity.kt
+++ app/src/main/java/com/upb/ticfinal/MainActivity.kt
@@ -2,7 +2,9 @@ package com.upb.ticfinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -20,14 +22,15 @@ class MainActivity : AppCompatActivity() {
         workWithUpdates()
     }
     private fun workWithUpdates(){
-            val parkingRef = database.getReference("parking")
+            val parkingRef = database.getReference("Test")
             parkingRef.addValueEventListener(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
-                    val temp = ArrayList<ParkingLot>()
+                    val temp = ArrayList<DataSnapshot>()
                     for(child in snapshot.children){
-                        child.getValue(ParkingLot::class.java)?.also { temp.add(it) }
+                        temp.add(child)
                     }
                     Toast.makeText(this@MainActivity,"$temp",Toast.LENGTH_LONG).show()
+                    updateScreen(temp)
                     print(temp)
                     //Todo hacer que reciba solo la ultima linea actualizada
                     // en el realtime database cambiar de formato y que solo lea p1:0  0 p5:1
@@ -40,4 +43,173 @@ class MainActivity : AppCompatActivity() {
 
             })
         }
+
+    private fun loadImage(imageView: ImageView,image:Int){
+        if (image ==1){
+            //Todo cargar la imagen con el auto
+        }else{
+            //Todo cargar la imgagen sin el auto
+        }
+        //imageView.setImageDrawable()
+    }
+    private fun updateScreen(temp: List<DataSnapshot>){
+        for (element in temp){
+            val state = (element.value as Long).toInt()
+            when(element.key){
+                "P1" -> {
+                    //loadImage(view,state)
+                    val sum = (element.value as Long).toInt() + 10
+                    Toast.makeText(this,"$sum",Toast.LENGTH_SHORT).show()
+                }
+                "P2" -> {
+
+                }
+                "P3" -> {
+
+                }
+                "P4" -> {
+
+                }
+                "P5" -> {
+
+                }
+                "P6" -> {
+
+                }
+                "P7" -> {
+
+                }
+                "P8" -> {
+
+                }
+                "P9" -> {
+
+                }
+                "P10" -> {
+
+                }
+                "P11" -> {
+
+                }
+                "P12" -> {
+
+                }
+                "P13" -> {
+
+                }
+                "P14" -> {
+
+                }
+                "P15" -> {
+
+                }
+                "P16" -> {
+
+                }
+                "P17" -> {
+
+                }
+                "P18" -> {
+
+                }
+                "P19" -> {
+
+                }
+                "P20" -> {
+
+                }
+                "P21" -> {
+
+                }
+                "P22" -> {
+
+                }
+                "P23" -> {
+
+                }
+                "P24" -> {
+
+                }
+                "P25" -> {
+
+                }
+                "P26" -> {
+
+                }
+                "P27" -> {
+
+                }
+                "P28" -> {
+
+                }
+                "P29" -> {
+
+                }
+                "P30" -> {
+
+                }
+                "P31" -> {
+
+                }
+                "P32" -> {
+
+                }
+                "P33" -> {
+
+                }
+                "P34" -> {
+
+                }
+                "P35" -> {
+
+                }
+                "P36" -> {
+
+                }
+                "P37" -> {
+
+                }
+                "P38" -> {
+
+                }
+                "P39" -> {
+
+                }
+                "P40" -> {
+
+                }
+                "P41" -> {
+
+                }
+                "P42" -> {
+
+                }
+                "P43" -> {
+
+                }
+                "P44" -> {
+
+                }
+                "P45" -> {
+
+                }
+                "P46" -> {
+
+                }
+                "P47" -> {
+
+                }
+                "P48" -> {
+
+                }
+                "P49" -> {
+
+                }
+                "50" -> {
+
+                }
+            }
+
+        }
+    }
 }
