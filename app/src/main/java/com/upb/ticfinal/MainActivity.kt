0df5bc74d628d177a94dd4e35a2de405c33296d1
Manu200162 <manli200134@gmail.com>
2022-12-17 11:20:21 -0400

    Final modification

--- app/src/main/java/com/upb/ticfinal/MainActivity.kt
+++ app/src/main/java/com/upb/ticfinal/MainActivity.kt
@@ -2,7 +2,9 @@ package com.upb.ticfinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
@@ -57,230 +59,71 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-    private fun loadImage(imageView: ImageView,s:Int,key:String){
+    private fun loadImage(imageView: ImageView,s:Int,key:String,textView: TextView){
         if(s != states.get(key)){
             if (s ==1){
                 imageView.setImageResource(R.drawable.occuped1)
+                textView.visibility = View.GONE
                 with(states) { put(key, 1) }
             }else{
                 imageView.setImageResource(R.drawable.empty)
+                textView.visibility = View.VISIBLE
                 with(states) { put(key, 0) }
             }
         }
     }
     private fun updateScreen(temp: List<DataSnapshot>){
-        if (temp.size == 50){
-            loadImage(binding.P1img,(temp[0].value as Long).toInt(),temp[0].key as String)
-            loadImage(binding.P2img,(temp[11].value as Long).toInt(),temp[11].key as String)
-            loadImage(binding.P3img,(temp[22].value as Long).toInt(),temp[22].key as String)
-            loadImage(binding.P4img,(temp[33].value as Long).toInt(),temp[33].key as String)
-            loadImage(binding.P5img,(temp[44].value as Long).toInt(),temp[44].key as String)
-            loadImage(binding.P6img,(temp[46].value as Long).toInt(),temp[46].key as String)
-            loadImage(binding.P7img,(temp[47].value as Long).toInt(),temp[47].key as String)
-            loadImage(binding.P8img,(temp[48].value as Long).toInt(),temp[48].key as String)
-            loadImage(binding.P9img,(temp[49].value as Long).toInt(),temp[49].key as String)
-            loadImage(binding.P10img,(temp[1].value as Long).toInt(),temp[1].key as String)
-            loadImage(binding.P11img,(temp[2].value as Long).toInt(),temp[2].key as String)
-            loadImage(binding.P12img,(temp[3].value as Long).toInt(),temp[3].key as String)
-            loadImage(binding.P13img,(temp[4].value as Long).toInt(),temp[4].key as String)
-            loadImage(binding.P14img,(temp[5].value as Long).toInt(),temp[5].key as String)
-            loadImage(binding.P15img,(temp[6].value as Long).toInt(),temp[6].key as String)
-            loadImage(binding.P16img,(temp[7].value as Long).toInt(),temp[7].key as String)
-            loadImage(binding.P17img,(temp[8].value as Long).toInt(),temp[8].key as String)
-            loadImage(binding.P18img,(temp[9].value as Long).toInt(),temp[9].key as String)
-            loadImage(binding.P19img,(temp[10].value as Long).toInt(),temp[10].key as String)
-            loadImage(binding.P20img,(temp[12].value as Long).toInt(),temp[12].key as String)
-            loadImage(binding.P21img,(temp[13].value as Long).toInt(),temp[13].key as String)
-            loadImage(binding.P22img,(temp[14].value as Long).toInt(),temp[14].key as String)
-            loadImage(binding.P23img,(temp[15].value as Long).toInt(),temp[15].key as String)
-            loadImage(binding.P24img,(temp[16].value as Long).toInt(),temp[16].key as String)
-            loadImage(binding.P25img,(temp[17].value as Long).toInt(),temp[17].key as String)
-            loadImage(binding.P26img,(temp[18].value as Long).toInt(),temp[18].key as String)
-            loadImage(binding.P27img,(temp[19].value as Long).toInt(),temp[19].key as String)
-            loadImage(binding.P28img,(temp[20].value as Long).toInt(),temp[20].key as String)
-            loadImage(binding.P29img,(temp[21].value as Long).toInt(),temp[21].key as String)
-            loadImage(binding.P30img,(temp[23].value as Long).toInt(),temp[23].key as String)
-            loadImage(binding.P31img,(temp[24].value as Long).toInt(),temp[24].key as String)
-            loadImage(binding.P32img,(temp[25].value as Long).toInt(),temp[25].key as String)
-            loadImage(binding.P33img,(temp[26].value as Long).toInt(),temp[26].key as String)
-            loadImage(binding.P34img,(temp[27].value as Long).toInt(),temp[27].key as String)
-            loadImage(binding.P35img,(temp[28].value as Long).toInt(),temp[28].key as String)
-            loadImage(binding.P36img,(temp[29].value as Long).toInt(),temp[29].key as String)
-            loadImage(binding.P37img,(temp[30].value as Long).toInt(),temp[30].key as String)
-            loadImage(binding.P38img,(temp[31].value as Long).toInt(),temp[31].key as String)
-            loadImage(binding.P39img,(temp[32].value as Long).toInt(),temp[32].key as String)
-            loadImage(binding.P40img,(temp[34].value as Long).toInt(),temp[34].key as String)
-            loadImage(binding.P41img,(temp[35].value as Long).toInt(),temp[35].key as String)
-            loadImage(binding.P42img,(temp[36].value as Long).toInt(),temp[36].key as String)
-            loadImage(binding.P43img,(temp[37].value as Long).toInt(),temp[37].key as String)
-            loadImage(binding.P44img,(temp[38].value as Long).toInt(),temp[38].key as String)
-            loadImage(binding.P45img,(temp[39].value as Long).toInt(),temp[39].key as String)
-            loadImage(binding.P46img,(temp[40].value as Long).toInt(),temp[40].key as String)
-            loadImage(binding.P47img,(temp[41].value as Long).toInt(),temp[41].key as String)
-            loadImage(binding.P48img,(temp[42].value as Long).toInt(),temp[42].key as String)
-            loadImage(binding.P49img,(temp[43].value as Long).toInt(),temp[43].key as String)
-            loadImage(binding.P50img,(temp[45].value as Long).toInt(),temp[45].key as String)
-
-
-        }
-
-        for (element in temp){
-            val state = (element.value as Long).toInt()
-            when(element.key){
-                "P1" -> {
-                    //loadImage(view,state)
-                    val sum = (element.value as Long).toInt() + 10
-                    Toast.makeText(this,"$sum",Toast.LENGTH_SHORT).show()
-                }
-                "P2" -> {
-
-                }
-                "P3" -> {
-
-                }
-                "P4" -> {
-
-                }
-                "P5" -> {
-
-                }
-                "P6" -> {
-
-                }
-                "P7" -> {
-
-                }
-                "P8" -> {
-
-                }
-                "P9" -> {
-
-                }
-                "P10" -> {
-
-                }
-                "P11" -> {
-
-                }
-                "P12" -> {
-
-                }
-                "P13" -> {
-
-                }
-                "P14" -> {
-
-                }
-                "P15" -> {
-
-                }
-                "P16" -> {
-
-                }
-                "P17" -> {
-
-                }
-                "P18" -> {
-
-                }
-                "P19" -> {
-
-                }
-                "P20" -> {
-
-                }
-                "P21" -> {
-
-                }
-                "P22" -> {
-
-                }
-                "P23" -> {
-
-                }
-                "P24" -> {
-
-                }
-                "P25" -> {
-
-                }
-                "P26" -> {
-
-                }
-                "P27" -> {
-
-                }
-                "P28" -> {
-
-                }
-                "P29" -> {
-
-                }
-                "P30" -> {
-
-                }
-                "P31" -> {
-
-                }
-                "P32" -> {
-
-                }
-                "P33" -> {
-
-                }
-                "P34" -> {
-
-                }
-                "P35" -> {
-
-                }
-                "P36" -> {
-
-                }
-                "P37" -> {
-
-                }
-                "P38" -> {
-
-                }
-                "P39" -> {
-
-                }
-                "P40" -> {
-
-                }
-                "P41" -> {
-
-                }
-                "P42" -> {
-
-                }
-                "P43" -> {
-
-                }
-                "P44" -> {
-
-                }
-                "P45" -> {
-
-                }
-                "P46" -> {
-
-                }
-                "P47" -> {
-
-                }
-                "P48" -> {
-
-                }
-                "P49" -> {
-
-                }
-                "50" -> {
-
-                }
-            }
-
+        if (temp.size == 50) {
+            loadImage(binding.P1img, (temp[0].value as Long).toInt(), temp[0].key as String,binding.P1tx)
+            loadImage(binding.P2img, (temp[11].value as Long).toInt(), temp[11].key as String,binding.P2tx)
+            loadImage(binding.P3img, (temp[22].value as Long).toInt(), temp[22].key as String,binding.P3tx)
+            loadImage(binding.P4img, (temp[33].value as Long).toInt(), temp[33].key as String,binding.P4tx)
+            loadImage(binding.P5img, (temp[44].value as Long).toInt(), temp[44].key as String,binding.P5tx)
+            loadImage(binding.P6img, (temp[46].value as Long).toInt(), temp[46].key as String,binding.P6tx)
+            loadImage(binding.P7img, (temp[47].value as Long).toInt(), temp[47].key as String,binding.P7tx)
+            loadImage(binding.P8img, (temp[48].value as Long).toInt(), temp[48].key as String,binding.P8tx)
+            loadImage(binding.P9img, (temp[49].value as Long).toInt(), temp[49].key as String,binding.P9tx)
+            loadImage(binding.P10img, (temp[1].value as Long).toInt(), temp[1].key as String,binding.P10tx)
+            loadImage(binding.P11img, (temp[2].value as Long).toInt(), temp[2].key as String,binding.P11tx)
+            loadImage(binding.P12img, (temp[3].value as Long).toInt(), temp[3].key as String,binding.P12tx)
+            loadImage(binding.P13img, (temp[4].value as Long).toInt(), temp[4].key as String,binding.P13tx)
+            loadImage(binding.P14img, (temp[5].value as Long).toInt(), temp[5].key as String,binding.P14tx)
+            loadImage(binding.P15img, (temp[6].value as Long).toInt(), temp[6].key as String,binding.P15tx)
+            loadImage(binding.P16img, (temp[7].value as Long).toInt(), temp[7].key as String,binding.P16tx)
+            loadImage(binding.P17img, (temp[8].value as Long).toInt(), temp[8].key as String,binding.P17tx)
+            loadImage(binding.P18img, (temp[9].value as Long).toInt(), temp[9].key as String,binding.P18tx)
+            loadImage(binding.P19img, (temp[10].value as Long).toInt(), temp[10].key as String,binding.P19tx)
+            loadImage(binding.P20img, (temp[12].value as Long).toInt(), temp[12].key as String,binding.P20tx)
+            loadImage(binding.P21img, (temp[13].value as Long).toInt(), temp[13].key as String,binding.P21tx)
+            loadImage(binding.P22img, (temp[14].value as Long).toInt(), temp[14].key as String,binding.P22tx)
+            loadImage(binding.P23img, (temp[15].value as Long).toInt(), temp[15].key as String,binding.P23tx)
+            loadImage(binding.P24img, (temp[16].value as Long).toInt(), temp[16].key as String,binding.P24tx)
+            loadImage(binding.P25img, (temp[17].value as Long).toInt(), temp[17].key as String,binding.P25tx)
+            loadImage(binding.P26img, (temp[18].value as Long).toInt(), temp[18].key as String,binding.P26tx)
+            loadImage(binding.P27img, (temp[19].value as Long).toInt(), temp[19].key as String,binding.P27tx)
+            loadImage(binding.P28img, (temp[20].value as Long).toInt(), temp[20].key as String,binding.P28tx)
+            loadImage(binding.P29img, (temp[21].value as Long).toInt(), temp[21].key as String,binding.P29tx)
+            loadImage(binding.P30img, (temp[23].value as Long).toInt(), temp[23].key as String,binding.P30tx)
+            loadImage(binding.P31img, (temp[24].value as Long).toInt(), temp[24].key as String,binding.P31tx)
+            loadImage(binding.P32img, (temp[25].value as Long).toInt(), temp[25].key as String,binding.P32tx)
+            loadImage(binding.P33img, (temp[26].value as Long).toInt(), temp[26].key as String,binding.P33tx)
+            loadImage(binding.P34img, (temp[27].value as Long).toInt(), temp[27].key as String,binding.P34tx)
+            loadImage(binding.P35img, (temp[28].value as Long).toInt(), temp[28].key as String,binding.P35tx)
+            loadImage(binding.P36img, (temp[29].value as Long).toInt(), temp[29].key as String,binding.P36tx)
+            loadImage(binding.P37img, (temp[30].value as Long).toInt(), temp[30].key as String,binding.P37tx)
+            loadImage(binding.P38img, (temp[31].value as Long).toInt(), temp[31].key as String,binding.P38tx)
+            loadImage(binding.P39img, (temp[32].value as Long).toInt(), temp[32].key as String,binding.P39tx)
+            loadImage(binding.P40img, (temp[34].value as Long).toInt(), temp[34].key as String,binding.P40tx)
+            loadImage(binding.P41img, (temp[35].value as Long).toInt(), temp[35].key as String,binding.P41tx)
+            loadImage(binding.P42img, (temp[36].value as Long).toInt(), temp[36].key as String,binding.P42tx)
+            loadImage(binding.P43img, (temp[37].value as Long).toInt(), temp[37].key as String,binding.P43tx)
+            loadImage(binding.P44img, (temp[38].value as Long).toInt(), temp[38].key as String,binding.P44tx)
+            loadImage(binding.P45img, (temp[39].value as Long).toInt(), temp[39].key as String,binding.P45tx)
+            loadImage(binding.P46img, (temp[40].value as Long).toInt(), temp[40].key as String,binding.P46tx)
+            loadImage(binding.P47img, (temp[41].value as Long).toInt(), temp[41].key as String,binding.P47tx)
+            loadImage(binding.P48img, (temp[42].value as Long).toInt(), temp[42].key as String,binding.P48tx)
+            loadImage(binding.P49img, (temp[43].value as Long).toInt(), temp[43].key as String,binding.P49tx)
+            loadImage(binding.P50img, (temp[45].value as Long).toInt(), temp[45].key as String,binding.P50tx)
         }
     }
 }
